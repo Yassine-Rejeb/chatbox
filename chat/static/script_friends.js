466e4c3b16ce3d29468ae529f21a03a6cc444cd7
MohamedYassine Rejeb <mohamedyassine.rejeb@gmail.com>
2022-12-19 00:46:17 +0100

    Disaster Bypassed

--- chat/static/script_friends.js
+++ chat/static/script_friends.js
@@ -1,93 +1,90 @@
-(function(){
-  var chat = {
-    init: function() {
-      this.cacheDOM();
-      this.bindEvents();
-      this.render();
-    },
-    cacheDOM: function() {
-      this.$chatHistory = $('.chat-history');
-      this.$button = $('button');
-      this.$textarea = $('#message-to-send');
-      this.$chatHistoryList =  this.$chatHistory.find('ul');
-    },
-    bindEvents: function() {
-      this.$button.on('click', this.addMessage.bind(this));
-      this.$textarea.on('keyup', this.addMessageEnter.bind(this));
-    },
-    render: function() {
-      this.scrollToBottom();
-      if (this.messageToSend.trim() !== '') {
-        var template = Handlebars.compile( $("#message-template").html());
-
-        var context = { 
-          messageOutput: this.messageToSend,
-          time: this.getCurrentTime()
-        };
-        
-        console.log('context');
-
-        this.$chatHistoryList.append(template(context));
-        this.scrollToBottom();
-        this.$textarea.val('');
-        
-        // responses
-        var templateResponse = Handlebars.compile( $("#message-response-template").html());
-        var contextResponse = { 
-          response: this.getRandomItem(this.messageResponses),
-          time: this.getCurrentTime()
-        };
-        
-        setTimeout(function() {
-          this.$chatHistoryList.append(templateResponse(contextResponse));
-          this.scrollToBottom();
-        }.bind(this), 1500);
-        
+// This script is used to fill the friend list with friends
+$(function() {
+  $.ajax({
+      type: "GET",
+      url: "friends/",
+      success: function(data) {
+        res= data
+        for (var i = 0; i < res.length; i++) {
+          var template = Handlebars.compile( $("#friend-template").html() );
+          var context={
+            friend: res[i],
+            profile_pic: '/chat/profile_pic/?username=' + res[i]
+          }
+            $("ul#friend-list").prepend(template(context));
       }
-      
-    },
-    
-    addMessage: function() {
-      this.messageToSend = this.$textarea.val()
-      this.render();         
-    },
-    addMessageEnter: function(event) {
-        // enter was pressed
-        if (event.keyCode === 13) {
-          this.addMessage();
+      },
+    });
+  return true;
+});
+
+// Make a get request to the server to add a friend
+$('#AddFriendBtn').click(function() {
+  $.ajax({
+      type: "GET",
+      url: "/chat/add_friend/?friend=" + $('#AddFriendInput').val(),
+      data: {
+        username: $('#AddFriendInput').val()
+      },
+      success: function(data) {
+        console.log($('#AddFriendInput').val());
+        console.log(data);
+        if (data == 'success') {
+          location.reload();
+        } 
+        else {
+          alert(data);
         }
-    },
-    scrollToBottom: function() {
-       this.$chatHistory.scrollTop(this.$chatHistory[0].scrollHeight);
-  },
-    getCurrentTime: function() {
-      return new Date().toLocaleTimeString().
-              replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
-    },
-    getRandomItem: function(arr) {
-      return arr[Math.floor(Math.random()*arr.length)];
-    }
-    
-  };
-  
-  chat.init();
-  
-  var searchFilter = {
-    options: { valueNames: ['name'] },
-    init: function() {
-      var userList = new List('people-list', this.options);
-      var noItems = $('<li id="no-items-found">No items found</li>');
-      
-      userList.on('updated', function(list) {
-        if (list.matchingItems.length === 0) {
-          $(list.list).append(noItems);
-        } else {
-          noItems.detach();
+      }
+  });
+  return false;
+});
+
+// Make a get request to the server to remove a friend
+$('#RemoveFriendBtn').click(function() {
+  $.ajax({
+      type: "GET",
+      url: "/chat/remove_friend/?friend=" + $('#AddFriendInput').val(),
+      data: {
+        username: $('#AddFriendInput').val()
+      },
+      success: function(data) {
+        console.log($('#AddFriendInput').val());
+        console.log(data);
+        if (data == 'success') {
+          location.reload();
         }
-      });
-    }
-  };
-  
-  searchFilter.init();
-  
-})();
+        else {
+          alert(data);
+        }
+      }
+  });
+  return false;
+});
+
+
+// Update the profile settings
+$('#UpdateProfileBtn').click(function() {
+  $.ajax({
+      type: "POST",
+      url: "/chat/update/",
+      data: {
+        profile_pic: $('#profile_pic').val(),
+        username: $('#username').val(),
+        new_password: $('#password1').val(),
+        new_password2: $('#password2').val(),
+        current_password: $('#password_old').val(),
+        csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+      },
+      success: function(data) {
+        console.log(data);
+        if (data == 'success') {
+          alert(data)
+        }
+        else {
+          alert(data);
+        }
+      }
+  });
+  return false;
+});
