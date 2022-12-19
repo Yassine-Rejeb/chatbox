b60f983ca7145cfb18b6ccf4eb8db0f79cba988e
MohamedYassine Rejeb <mohamedyassine.rejeb@gmail.com>
2022-12-19 18:56:21 +0100

    Stage1: [Up to chatting] DONE

---
+++ README.md
@@ -0,0 +1,36 @@
+# ChatBox
+
+A simple messenger WebApp made with Django.
+It includes :
+- Registration
+- Login
+- Chat UI
+Storage solution: MongoDB
+
+## Installation
+
+1. Install python3, django
+2. Install mongoDB
+3. Clone repo
+
+## Usage
+
+Open terminal under the repo. 
+`$ python manage.py runserver`
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+
+## Credits
+
+This project has been developped by chxmxii, [REDACTED] and myself(M0D4S).
+
+## License
+
+this project is under GPLv3.
--- chat/static/script.js
+++ chat/static/script.js
@@ -1,99 +1,24 @@
-(function(){
-  var chat = {
-    messageToSend: '',
-    messageResponses: [
-      'This is a sample response.',
-      'Python project using django.',
-      'This is a possible response',
-    ],
-    init: function() {
-      this.cacheDOM();
-      this.bindEvents();
-      this.render();
-    },
-    cacheDOM: function() {
-      this.$chatHistory = $('.chat-history');
-      this.$button = $('button');
-      this.$textarea = $('#message-to-send');
-      this.$chatHistoryList =  this.$chatHistory.find('ul');
-    },
-    bindEvents: function() {
-      this.$button.on('click', this.addMessage.bind(this));
-      this.$textarea.on('keyup', this.addMessageEnter.bind(this));
-    },
-    render: function() {
-      this.scrollToBottom();
-      if (this.messageToSend.trim() !== '') {
-        var template = Handlebars.compile( $("#message-template").html());
+//--------------- Floating settings window --------------->
+    // Get a reference to the button and the floating window
+    var toggleButton = document.getElementById('toggle-floating-window');
+    var floatingWindow = document.getElementById('floating-window');
 
-        var context = { 
-          messageOutput: this.messageToSend,
-          time: this.getCurrentTime()
-        };
-        
-        console.log('context');
+    // Add an event listener to the button that will toggle the floating window
+    toggleButton.addEventListener('click', function() {
+    // If the floating window is currently hidden, remove the 'hidden' class to show it
+    if (floatingWindow.classList.contains('hidden')) {
+        floatingWindow.classList.remove('hidden');
+    }
+    // If the floating window is currently visible, add the 'hidden' class to hide it
+    else {
+        floatingWindow.classList.add('hidden');
+    }
+    });
+//--------------- End Floating settings window --------------->
 
-        this.$chatHistoryList.append(template(context));
-        this.scrollToBottom();
-        this.$textarea.val('');
-        
-        // responses
-        var templateResponse = Handlebars.compile( $("#message-response-template").html());
-        var contextResponse = { 
-          response: this.getRandomItem(this.messageResponses),
-          time: this.getCurrentTime()
-        };
-        
-        setTimeout(function() {
-          this.$chatHistoryList.append(templateResponse(contextResponse));
-          this.scrollToBottom();
-        }.bind(this), 1500);
-        
-      }
-      
-    },
-    
-    addMessage: function() {
-      this.messageToSend = this.$textarea.val()
-      this.render();         
-    },
-    addMessageEnter: function(event) {
-        // enter was pressed
-        if (event.keyCode === 13) {
-          this.addMessage();
-        }
-    },
-    scrollToBottom: function() {
-       this.$chatHistory.scrollTop(this.$chatHistory[0].scrollHeight);
-  },
-    getCurrentTime: function() {
-      return new Date().toLocaleTimeString().
-              replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
-    },
-    getRandomItem: function(arr) {
-      return arr[Math.floor(Math.random()*arr.length)];
-    }
-    
-  };
-  
-  chat.init();
-  
-  var searchFilter = {
-    options: { valueNames: ['name'] },
-    init: function() {
-      var userList = new List('people-list', this.options);
-      var noItems = $('<li id="no-items-found">No items found</li>');
-      
-      userList.on('updated', function(list) {
-        if (list.matchingItems.length === 0) {
-          $(list.list).append(noItems);
-        } else {
-          noItems.detach();
-        }
-      });
-    }
-  };
-  
-  searchFilter.init();
-  
-})();
+
+// Hide the floating window
+function hideFloatingWindow() {
+    document.getElementById("floatingWindow").style.display = "none";
+} 
+
